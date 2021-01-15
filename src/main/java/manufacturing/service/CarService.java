2a52e20f3e114aace103dbf1e130974f2b6e197a
ivanluchkin <id.luchkin@gmail.com>
2021-01-15 22:44:17 +0200

    Code style fix

--- src/main/java/manufacturing/service/CarService.java
+++ src/main/java/manufacturing/service/CarService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import manufacturing.model.Car;
 import manufacturing.model.Driver;
 
-public interface CarService extends AbstractService<Car>{
+public interface CarService extends AbstractService<Car> {
     void addDriverToCar(Driver driver, Car car);
 
     void removeDriverFromCar(Driver driver, Car car);
